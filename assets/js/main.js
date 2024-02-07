96b5985e91e95110212ddf18b465efe4f4acb357
Byron-Gomez <byronxgomez127@gmail.com>
2024-02-06 19:20:34 -0600

    se actualizaron apis

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,12 +29,12 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 const sections = document.querySelectorAll("section[id]");
 
 function scrollActive() {
-  const scrollY = window.pageYOffset;
+  const scrollY = window.scrollY;
 
   sections.forEach((current) => {
     const sectionHeight = current.offsetHeight;
     const sectionTop = current.offsetTop - 50;
-    sectionId = current.getAttribute("id");
+   let sectionId = current.getAttribute("id");
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
       document
@@ -120,7 +120,6 @@ let opt = {
     format: "a4",
     orientation: "portrait",
     unit: "cm",
-    format: [21, 29.78]
   }
 };
 
